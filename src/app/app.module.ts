7ba91018d3ecc142367d2323bad8a7d2c6d9dc0d
Khoi <khoi.ntm22707@sinhvien.hoasen.edu.vn>
2023-02-20 01:16:05 +0700

    20/2

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CalculatorComponent } from './caculator/caculator.component';
+import { StoreModule } from '@ngrx/store';
+import { calculatorReducer } from 'src/ngrx/reducers/calculator.reducer';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalculatorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({
+      calculator: calculatorReducer
+    }, {})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
